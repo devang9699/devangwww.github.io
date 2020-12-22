2d41eb8de1d093c5b8818561868f49faaa3f5c94
devang9699 <48198953+devang9699@users.noreply.github.com>
2020-12-22 14:27:51 +0530

    Add files via upload

---
+++ website/custom.js
@@ -0,0 +1,106 @@
+$(document).ready(function(){
+
+	$('.slick-slider').slick({
+		autoplay:true,
+		autoplaySpeed: 1000,
+		dots: true,
+		infinite: false,
+		slidesToShow: 1,
+		slidesToScroll: 1,
+		centerMode:false,
+		arrows:false
+
+
+	}); 
+
+
+
+
+
+
+	$('.slick-text-slider').slick({
+		arrows:true,
+		autoplay:true,
+		autoplaySpeed:1000,
+		slidesToShow: 1,
+		slidesToScroll: 1
+
+		
+
+	}); 
+
+
+
+	var url=window.location.href;
+	url = url.substr(url.lastIndexOf("/") + 1);
+	
+
+	
+	$('.navbar ul li ').each(function(){
+
+
+		 var href = $(this).find('a').attr('href');
+		 if(url == href){
+
+			 $( this ).parent().find( 'li.active' ).removeClass( 'active' );
+			 $( this ).addClass( 'active' ); 	
+		 	
+		 }
+		});
+
+
+	
+
+
+		$(".accordion .question a").on("click", function(){
+			if ($(this).hasClass("active")){
+	  			console.log("if clicked");
+	  			$(this).removeClass("active");
+	  			$(this).parent().siblings(".content").slideUp(200);
+	  
+	 	    	$(".accordion .question a i").removeClass("fa-minus").addClass("fa-plus");
+			
+			}else{
+	 			 console.log("else clicked");
+	  			$(".accordion .question a i").removeClass("fa-minus").addClass("fa-plus");
+	  			$(this).find("i").removeClass("fa-plus").addClass("fa-minus");  
+
+	  			$(".accordion .question a").removeClass("active");
+	 		 	$(this).addClass("active");
+	  
+	  			$(".content").slideUp(200);
+	 			$(this).parent().siblings(".content").slideDown(200);
+			}
+		});
+  		
+  		$('.carousel-slider').slick({
+		
+		slidesToShow: 5,
+		slidesToScroll: 1,
+		infinite:true
+		
+		
+
+	
+
+
+	}); 
+
+
+
+  	});
+
+
+
+
+
+
+
+
+ 				
+
+
+
+
+
+
---
+++ website/header.php
@@ -0,0 +1,130 @@
+	<!DOCTYPE html>
+	<html>
+	<head>
+		<title>
+		</title>
+		<link rel="stylesheet" type="text/css" href="css/style.css">
+		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.css" crossorigin="anonymous">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<link rel="stylesheet" type="text/css" href="slick/slick.css"/>
+		<link rel="stylesheet" type="text/css" href="slick/slick-theme.css"/>
+		 <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/meyer-reset/2.0/reset.min.css">
+
+	</head>
+	<body>
+
+		<div class="request-quote-section">
+			<button><a href="https://www.conceptopensource.com/request-quote">
+				
+			</a>Request Quote
+			</button>
+		</div>
+
+		<div class="we-hiring-section">
+			<button><a href="https://www.conceptopensource.com/request-quote">
+				
+			</a>We are Hiring
+			</button>
+		</div>
+
+
+
+		<div id="header">
+			<div class="top-head">
+				<div class="container">
+					<div class="inner-header">
+						<div class="email-sec">
+							<a class="email_section" href="mailto:sales#conceptopensource.com">
+								<i class="fa fa-envelope"></i>
+								sales@conceptopensource.com
+							</a>
+						</div>
+						<div class="contact-sec">
+							<a class="phone-call"><i class="fa fa-phone"></i>
+								+918780882871(INDIA)
+							</a>
+
+							<a class="phone-call"><i class="fa fa-phone"></i>
+								+918488956864(USA)
+							</a> 
+						</div>
+					</div>
+				</div>
+			</div>
+
+			<div class="navigation-sec">
+				<div class="container">
+					<div class="logo-menus">
+						<div class="logosection">
+
+							<a href="https://conceptopensource.com">
+								<img id="logo" src="https://www.conceptopensource.com/wp-content/themes/conceptopensource/images/logo.png">
+							</a>
+						</div>
+
+						<div class="navbar">
+
+							<ul class="nav-list">
+
+								<li  class="active"><a href="index.php">Home</a></li>
+
+
+								<li class="dropdown">
+								
+									Developement
+										<ul class="dropdown-content">
+										
+										<li><a href="#">Web Development</a></li>
+										<li><a href="#">Cms Developement</a></li>
+										<li><a href="#">Ecommerce Web Developement</a></li>
+									</ul>
+								</li>
+								
+								<li class="dropdown">
+									
+									Design
+									<ul class="dropdown-content">
+										<li><a href="#">Logo Design</a></li>
+										<li><a href="#">Web Design</a></li>
+										<li><a href="#">Internet Marketing Services</a></li>
+									</ul>
+
+								</li>
+
+
+								<li class="dropdown">
+									Hire Developers
+									<ul class="dropdown-content">
+										<li><a href="#">Hire Php Developer</a></li>
+										<li><a href="#">Hire Magneto Developer</a></li>
+										<li><a href="#">Hire Frontend Developer</a></li>
+									</ul>
+
+								</li>
+
+
+
+								<li class="dropdown">
+									Mobile Development
+									<ul class="dropdown-content">
+										<li><a href="#">Mobile App Development</a></li>
+										<li><a href="#">Ios App Development</a></li>
+
+									</ul>
+
+								</li>
+
+								<li class="nav-li"><a href="testimonials.php">Testimonials</a></li>
+								<li class="nav-li">Portfolio</li>
+								<li  class="nav-li">Company</li>
+								<li  class="nav-li"><a href="FAQ.php">FAQ</a></li>
+								<li  class="nav-li"><a href="contact-us.php">Contact us</a></li>
+
+							</ul>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+
+			
